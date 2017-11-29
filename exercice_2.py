eaf438a42097eaaa8429a62a07b50a3e071968b3
Maxime Laramee <laramee.maxime@outlook.com>
2017-11-28 22:30:31 -0500

    Added Exercice 2 part 2

--- exercice_2.py
+++ exercice_2.py
@@ -1,24 +1,29 @@
 from abc import ABCMeta
 
 class Product:
+
     __metaclass__ = ABCMeta
     _product_name = "Base Product"
     _unit_price = 0
+    _is_free = False
 
     def get_price(self):
-        return self._unit_price
+        return 0 if self._is_free else self._unit_price
+
+    def set_free(self):
+        self._is_free = True
 
 class Pomme(Product):
+
     _product_name = "Pomme"
     _unit_price = 60
 
-
 class Orange(Product):
+
     _product_name = "Orange"
     _unit_price = 125
 
 class Cart:
-
 
     def __init__(self):
         self._products = []
@@ -36,3 +41,46 @@ class Cart:
             value += self._products[i].get_price()
 
         return value
+
+    def apply_offer(self, offer):
+
+        if not isinstance(offer, Offre):
+            return
+
+        offer.process(self._products)
+
+class Offre:
+
+    __metaclass__ = ABCMeta
+    _product_class = None
+    _nth_free = -1
+
+    def __init__(self, product_class, nth_free):
+        self._product_class = product_class
+        self._nth_free = nth_free
+
+    def process(self, products):
+        product_count = 0
+        for i in range(len(products)):
+
+            if products[i]._product_name == self._product_class:
+ 
+                product_count += 1
+                if not product_count % (self._nth_free):
+                    products[i].set_free()
+
+class OffrePomme(Offre):
+
+    def __init__(self):
+        Offre.__init__(self, "Pomme", 2)
+
+    def process(self, products):
+        Offre.process(self, products)
+
+class OffreOrange(Offre):
+
+    def __init__(self):
+        Offre.__init__(self, "Orange", 3)
+
+    def process(self, products):
+        Offre.process(self, products)
--- tests_exercice_2.py
+++ tests_exercice_2.py
@@ -1,5 +1,6 @@
 import unittest
 from exercice_2 import Pomme, Orange, Cart
+from exercice_2 import OffrePomme, OffreOrange
  
 class TestUM(unittest.TestCase):
  
@@ -7,6 +8,14 @@ class TestUM(unittest.TestCase):
 
         self.test_matrix = [
             [[Pomme(), Pomme(), Orange(), Pomme()], 3.05],
+        ]
+
+        self.test_offers_matrix = [
+            [[Pomme(), Pomme(), Orange(), Pomme()], [OffrePomme(), OffreOrange()], 2.45],
+            [[Pomme(), Pomme()], [OffrePomme(), OffreOrange()], 0.6],
+            [[Pomme(), Pomme(), Orange(), Pomme(), Orange(), Orange() ], [OffrePomme(), OffreOrange()], 3.7],
+            [[Pomme(), Orange() ], [OffrePomme(), OffreOrange()], 1.85],
+            [[], [OffrePomme(), OffreOrange()], 0],
         ]
  
     def test_matrix(self):
@@ -17,9 +26,27 @@ class TestUM(unittest.TestCase):
             for j in range(len(products)):
                 
                 cart.add_product(products[j])
-
+           
             # Conversion de cents vers dollars
             self.assertEqual(cart.get_cart_value()/100.0, self.test_matrix[i][1])
+
+    def test_offers_matrix(self):
+
+        for i in range(len(self.test_offers_matrix)): 
+            cart = Cart()
+            products = self.test_offers_matrix[i][0]
+            offers = self.test_offers_matrix[i][1]
+
+            # Ajoute les produits au panier
+            for j in range(len(products)):
+                cart.add_product(products[j])
+
+            # Applique les offres
+            for j in range(len(offers)):
+                cart.apply_offer(offers[j])
+           
+            # Conversion de cents vers dollars
+            self.assertEqual(cart.get_cart_value()/100.0, self.test_offers_matrix[i][2])
  
 if __name__ == '__main__':
     unittest.main()
